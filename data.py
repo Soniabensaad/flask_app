e4f6d5a11c306872f754d3fb184789091e21b7eb
Soniabensaad <5687@holbertonstudents.com>
2023-03-12 21:48:54 +0100

    blog flask

---
+++ data.py
@@ -0,0 +1,27 @@
+def Posts():
+    posts = [
+        {
+          'id' :1,
+          'title': 'ABOUT ME',
+          'body' : 'I  the world of graphics, webdata, started retraing this year in this field, moved from hydraulic engineering,using several programs to become in',
+          'author' : 'Sonia Ben Saâd',
+          'data': '12.03.2023'
+        },
+        {
+          'id' :2,
+          'title' : 'POEMS',
+          'body' : 'I started writing on an early age it was about 12 years old , i write in 3 languages , tunisian dialect, french and english , the main subject is about love and life',
+          'author' : 'Sonia Ben Saâd',
+          'data': '12.03.2023'
+        
+        },
+        {
+          'id' : 3,
+          'title' : 'IT DREAMER',
+          'body' : 'Currently i am studying software engineering and i appreciate it move from langiuage to another , a new start at the age of 31',
+          'author' : 'Sonia Ben Saâd',
+          'data': '12.03.2023'
+        }
+         
+    ]
+    return posts
